03373dfec828099ee1de755b825a67ec255e8ac0
YOOGOMJA <machun3@naver.com>
2017-04-16 20:37:35 +0900

    DATABASE ENCAPSULATION

--- app.js
+++ app.js
@@ -12,12 +12,10 @@ var mysql = require('mysql');
 
 var app = express();
 
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+ 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -26,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/'  , index);
 app.use('/users', users);
 
 // BOWER PATH SETUP
