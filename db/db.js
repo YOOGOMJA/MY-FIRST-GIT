ad70b4d09be26875b42a04ea39ba719cfee02a3f
YOOGOMJA <machun3@naver.com>
2017-04-26 20:16:33 +0900

    sequelize
    Signed-off-by: YOOGOMJA <machun3@naver.com>

--- db/db.js
+++ db/db.js
@@ -2,14 +2,6 @@ var express = require('express');
 var mysql = require('mysql');
 var q = require('q');
 
-// var conn = mysql.createConnection({
-//   host : 'east-mariadb-dev.cpvfhejojors.ap-northeast-2.rds.amazonaws.com',
-//   port : 3306,
-//   user : 'gomja',
-//   password : 'rhawk1202:-)',
-//   database : 'DEV'
-// });
-
 var pool= mysql.createPool({
     host : 'east-mariadb-dev.cpvfhejojors.ap-northeast-2.rds.amazonaws.com',
     port : 3306,
